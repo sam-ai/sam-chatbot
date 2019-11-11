e6cd3af0fc76843a56f496eecd43db0b7457efb0
samV2 <sambit@nirvedacognition.ai>
2019-11-11 15:48:31 +0530

    chatbot app first commit

---
+++ android/app/src/main/kotlin/com/sam/flutter_chatapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sam.flutter_chatapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
